3ffcc9a38685c0ccebb0092fb0f30a9d60acfb2e
Barry1003 <isholausaamah@gamil.com>
2024-01-23 17:11:15 +0100

    jfd

--- src/utils/Nav/Sellers_nav.jsx
+++ src/utils/Nav/Sellers_nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { FiSearch } from "react-icons/fi";
 const Sellers_nav = () => {
   return (
     <div>
-      
+        <div className="flex sticky bg-slate-100 items-between justify-center">
+        <div className="w-full items-center  flex">
+                <img src={logo} className='w-[70%] object-contain' alt="" />
+                <p className='text-[34px] font-[800] '>Serve+ </p>
+            </div> 
+              <div className="flex justify-between items-center w-full bg-[rgba(217, 217, 217, 0.34)] rounded-full" style={{"border":"1.5px solid #7C7873 "}}>
+                  <input type="search"  className='w-full ' placeholder='Type to search.....'/>
+                  <FiSearch />
+              </div>
+              <div className=""></div>
+        </div>
     </div>
   )
 }
